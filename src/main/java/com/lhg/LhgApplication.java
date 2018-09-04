0bc1dffec4de3fadda4016f704b83ce36153b222
임호균 <flaghrbs1004@naver.com>
2018-09-04 10:33:14 +0900

    test

---
+++ src/main/java/com/lhg/LhgApplication.java
@@ -0,0 +1,12 @@
+package com.lhg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LhgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LhgApplication.class, args);
+	}
+}
